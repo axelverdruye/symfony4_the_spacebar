0c2f86e8bf3736f6f8140d9c414e630d0d6ba759
axelverdruye <axel@streamovations.be>
2018-09-13 18:01:23 +0200

    added database access and custom filters

---
+++ skeleton/src/Controller/ArticleAdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ArticleAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/article/new")
+     */
+    public function new(EntityManagerInterface $em)
+    {
+        $article = new Article();
+        $article->setTitle('this is a new title')
+                ->setSlug('why-asteroids-taste-like-bacon-'.rand(100, 999))
+                ->setContent("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
+
+        if (rand(1, 10) > 2) {
+            $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $em->persist($article);
+        $em->flush();
+
+        return new Response(sprintf(
+          'Hiya! New Article id: #%d slug: %s',
+          $article->getId(),
+          $article->getSlug()
+        ));
+    }
+}
